94f84c46cc4986a00203c5e1185331da2cfbe5e2
John <johnhkim124@yahoo.com>
2020-12-10 10:35:40 -0500

    css nav

--- fetched/src/components/Nav.jsx
+++ fetched/src/components/Nav.jsx
@@ -12,42 +12,48 @@ function Nav() {
         <Link to="/">Home</Link>
         <Link to="/dogListing">Adopt</Link>
         <Link to="/newDogForm">Find a Home</Link>
-        <div>
-          <Link>Sign Up</Link>
-          <Link>Log In</Link>
-        </div>
+
+        <Link>Sign Up</Link>
+        <Link>Log In</Link>
       </div>
     );
   }
 
   return (
     <div className="nav">
+      <header>
+        <div className="logoDiv">
+          <Link className="fetchedLogo" to="/">
+            Fetched
+          </Link>
+        </div>
 
-  <header>
-    <div className="logoDiv">
-      <Link className="fetchedLogo" to="/">
-        Fetched
-      </Link>
-    </div>
+        <div className="regularNav">
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/dogListing">Adopt</Link>
+              </li>
+              <li>
+                <Link to="/newDogForm">Find a Home</Link>
+              </li>
+              <li>
+                <Link>Sign Up</Link>
+              </li>
+              <li>
+                <Link>Log In</Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
 
-      <div className="regularNav">
-       <nav>
-        <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/dogListing">Adopt</Link></li>
-        <li><Link to="/newDogForm">Find a Home</Link></li>
-        <li><Link>Sign Up</Link></li>
-        <li><Link>Log In</Link></li>
-        </ul>
-        </nav>
-
-
-      </div>
-
-      <span className="hamburgerNav" onClick={() => setOpenNav(!openNav)}>
-        Menu
-      </span>
-      {navMenu}
+        <span className="hamburgerNav" onClick={() => setOpenNav(!openNav)}>
+          Menu
+        </span>
+        {navMenu}
       </header>
     </div>
   );
